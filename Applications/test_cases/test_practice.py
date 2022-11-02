81f6cde6f4bfdde1b3fa50b42b95c47214d46ab3
MdPorag <mdporagbsmrstu@gmail.com>
2022-11-02 17:06:56 +0600

    Update

--- Applications/test_cases/test_practice.py
+++ Applications/test_cases/test_practice.py
@@ -1,22 +1,31 @@
 import time
+from telnetlib import EC
 
+import pytest
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.core import driver
 from Src.Locators.locators import Locator
 
 from Src.Page_object_model.pom_loginPage import LogInPage
-from Src.Page_object_model.pom_ApplicationPage import CreateApplicationPage
 from Src.screen_shots.screen_shots import SS
 from Src.base.environment_setup import EnvironmentSetup
-from selenium.common.exceptions import NoSuchElementException
 from urllib.request import urlopen
 from urllib.error import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-ss_path = "/Applications/GoLang/"
+ss_path = "/Applications/PHP/"
 
 
-class TestCreateGolang(EnvironmentSetup):
-
-    def test1(self):
+class TestCreatePHP(EnvironmentSetup):
+    def test_Laravel_default_01(self):
+        # pytest.skip("Skipping test...later I will implement...")
         pageUrl = "https://eks.rakibefstestmaincluster782.klovercloud.io/"
         driver = self.driver
 
@@ -40,15 +49,15 @@ class TestCreateGolang(EnvironmentSetup):
         self.driver.implicitly_wait(20)
         time.sleep(2)
         # ******************************Login**********************************
-
+        print("----------------------Cluster LogIn-----------------------------------")
         # page object
         log = LogInPage(driver)  # LogIn page
 
         # input email
         try:
             if log.Email_box.is_enabled():
-                log.Email_box.send_keys('admin@klovercloud.com')
                 print("Email box is enabled")
+                log.Email_box.send_keys('admin@klovercloud.com')
                 time.sleep(1)
             else:
                 print("Email box is not enabled")
@@ -61,8 +70,8 @@ class TestCreateGolang(EnvironmentSetup):
         # input pass
         try:
             if log.Password_box.is_enabled():
-                log.Password_box.send_keys('Hello@1234')
                 print("Password box is enabled")
+                log.Password_box.send_keys('Hello@1234')
                 time.sleep(1)
             else:
                 print("Password box is not enabled")
@@ -74,10 +83,10 @@ class TestCreateGolang(EnvironmentSetup):
         # To show password
         try:
             if log.Toggle_Visibility_Button.is_enabled():
+                print("Toggle Visibility Button box is enabled")
                 log.Toggle_Visibility_Button.click()
                 time.sleep(1)
                 log.Toggle_Visibility_Button.click()
-                print("Password box is enabled")
                 time.sleep(1)
             else:
                 print("Password box is not enabled")
@@ -89,11 +98,10 @@ class TestCreateGolang(EnvironmentSetup):
         # Click On SignIn button
         try:
             if log.Sign_In_button.is_enabled():
+                print("Sign In button is clickable")
                 log.Sign_In_button.click()
                 self.driver.implicitly_wait(10)
-                time.sleep(5)
-                print("Sign In button is clickable")
-                time.sleep(2)
+                time.sleep(7)
             else:
                 print("Sign In button is not clickable")
         except NoSuchElementException as e:
@@ -101,185 +109,190 @@ class TestCreateGolang(EnvironmentSetup):
         else:
             print('Successfully logged in')
 
+        print("****************** Create PHP Laravel Application based on Team: default *********************")
         # click on create button from header
-        CreateNew_H = self.driver.find_element(By.XPATH, Locator.CreateNew_H)
+
+        print("-----------------------Header frame----------------------------------------")
+        # click on create button from header
         try:
-            if CreateNew_H.is_enabled():
-                CreateNew_H.click()
-                self.driver.implicitly_wait(10)
-                print("CreateNew_H button is clickable")
-                time.sleep(2)
-            else:
-                print("CreateNew_H In button is not clickable")
+            CreateNew_H = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, Locator.CreateNew_H)))
+            CreateNew_H.click()
+            time.sleep(2)
+            # if CreateNew_H.is_enabled():
+            #     print("CreateNew_H button is enable")
+            #     CreateNew_H.click()
+            #     time.sleep(2)
+            # else:
+            #     print("CreateNew_H In button is not enable")
         except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
+            print("NoSuchElementException error :\n", e, "\n")
         else:
-            print('Successfully clicked on CreateNew_H')
+            print('Successfully clicked on CreateNew')
 
         # click on "New Application" button from dropdown
-        NewApplication_H = self.driver.find_element(By.XPATH, Locator.NewApplication_H)
         try:
-            if NewApplication_H.is_enabled():
-                NewApplication_H.click()
-                self.driver.implicitly_wait(10)
-                time.sleep(5)
-                print("NewApplication_H button is clickable")
-            else:
-                print("NewApplication_H In button is not clickable")
+            NewApplication_H = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, Locator.NewApplication_H)))
+            NewApplication_H.click()
+            time.sleep(5)
+            # if NewApplication_H.is_enabled():
+            #     print("NewApplication_H button is enable")
+            #     NewApplication_H.click()
+            #     self.driver.implicitly_wait(30)
+            #     driver.refresh()
+            #     time.sleep(1)
+            # else:
+            #     print("NewApplication_H In button is not clickable")
         except NoSuchElementException as e:
             print("NoSuchElementException error", e)
         else:
             print('Successfully clicked on NewApplication_H')
-
-        # Choose GoLang
-        GoLang = self.driver.find_element(By.XPATH, Locator.GoLang)
+        print("-----------------------Welcome Create Application Page----------------------------------------")
+        print("----Create Python app with PHP Version: 7.3 &  Laravel version : 7.0--------")
+        # choose Django
+        # app = CreateApplicationPage(self.driver)
         try:
-            if GoLang.is_enabled():
-                GoLang.click()
-                self.driver.implicitly_wait(10)
-                time.sleep(5)
-                print("GoLang button is clickable")
-                time.sleep(2)
-            else:
-                print("GoLang button is not clickable")
+            Laravel = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, Locator.Laravel)))
+            Laravel.click()
+            # if Laravel.is_displayed():
+            #     print("Laravel button is displayed")
+            #     Laravel.click()
+            #     time.sleep(2)
+            # else:
+            #     print("Laravel is not displayed below")
         except NoSuchElementException as e:
             print("NoSuchElementException error", e)
         else:
-            print('Successfully chose on GoLang')
-
-        # Put Application Name
-        ApplicationName_box = driver.find_element(By.XPATH, Locator.ApplicationName_box)
-        try:
-            if ApplicationName_box.is_enabled():
-                ApplicationName_box.send_keys("testGo")
-                self.driver.implicitly_wait(10)
-                time.sleep(2)
-                print("ApplicationName_box is enable")
-            else:
-                print("ApplicationName_box is not enable")
-        except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
-        else:
-            print('Successfully inputted application name')
-
-        # Click On Team box
-        TeamBox_A = driver.find_element(By.XPATH, Locator.TeamBox_A)
-        try:
-            if TeamBox_A.is_enabled():
-                TeamBox_A.click()
-                self.driver.implicitly_wait(10)
-                time.sleep(2)
-                print("TeamBox_A is enable")
-            else:
-                print("TeamBox_A is not enable")
-        except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
-        else:
-            print('Successfully clicked on TeamBox')
-
-        # Choose Default from team
-        Team_Default = self.driver.find_element(By.XPATH, Locator.Team_Default)
-        try:
-            if Team_Default.is_displayed():
-                Team_Default.click()
-                self.driver.implicitly_wait(10)
-                time.sleep(2)
-                print("Team_Default is displayed")
-            else:
-                print("Team_Default is not displayed")
-        except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
-        else:
-            print('Successfully chose on Team_Default')
-
-        #  click next button
-        Next_button = self.driver.find_element(By.XPATH, Locator.Next_button)
-        try:
-            if Next_button.is_enabled():
-                Next_button.click()
-                self.driver.implicitly_wait(10)
-                print("Next_button is enable")
-                time.sleep(2)
-            else:
-                print("Next_button is not displayed")
-        except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
-        else:
-            print('Successfully clicked on Next_button')
-
-        #  again click next button
-        Next_button_two = self.driver.find_element(By.XPATH, Locator.Next_button)
-        try:
-            if Next_button_two.is_enabled():
-                Next_button_two.click()
-                self.driver.implicitly_wait(10)
-                print("Next_button is enable")
-                time.sleep(2)
-            else:
-                print("Next_button is not displayed")
-        except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
-        else:
-            print('Successfully clicked on Next_button')
-
-        # scroll below to show Namespaces
-        driver.execute_script("document.querySelector('.sidenav-content').scrollTop = 250")
-        print("Scroll down to show Namespaces")
-        time.sleep(2)
-
-        # Choose A Namespace for Prod Environment
-        Choose_Namespace_one = self.driver.find_element(By.XPATH, Locator.Choose_Namespace_one)
-        try:
-            if Choose_Namespace_one.is_enabled():
-                Choose_Namespace_one.click()
-                print("Choose_Namespace_one is selected")
-                time.sleep(5)
-            else:
-                print("Choose_Namespace_one is not enable")
-        except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
-        else:
-            print('Successfully chose Choose_Namespace_one')
-
-        # again scroll below
-        driver.execute_script("document.querySelector('.sidenav-content').scrollTop = 1200")
-        print("Scroll down to show Namespaces")
-        time.sleep(2)
-
-        # click on save button
-        Save_button = self.driver.find_element(By.XPATH, Locator.Save_button_A)
-        try:
-            if Save_button.is_enabled():
-                Save_button.click()
-                print("Save button is enable")
-                time.sleep(2)
-            else:
-                print("Save button is not enable")
-            time.sleep(2)
-        except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
-        else:
-            print('Successfully clicked on Save_button')
-
-        # take action's screenshot
-        ss = SS(driver)
-        file_name = ss_path + "GoLang_scrrenshot_" + time.asctime().replace(":", "_") + ".png"
-        ss.driver.save_screenshot(file_name)
-        ss.ScreenShot(file_name)
-
-
-"""
-        # click on Create application button
-        try:
-            Create_Application = self.driver.find_element(By.XPATH, Locator.Create_Application)
-            if Create_Application.is_enabled():
-                Create_Application.click()
-                print("Create application button is enable")
-                time.sleep(180)
-            else:
-                print("Create application button is not enable")
-        except NoSuchElementException as e:
-            print("NoSuchElementException error", e)
-        else:
-            print('Successfully clicked on Create application')
-"""
+            print('Successfully chose on Laravel')
+        #
+        # # put application name
+        # try:
+        #     ApplicationName_box = driver.find_element(By.XPATH, Locator.ApplicationName_box)
+        #     if ApplicationName_box.is_enabled():
+        #         print("ApplicationName_box is enable")
+        #         ApplicationName_box.send_keys('test_Laravel_default_01')
+        #         time.sleep(2)
+        #     else:
+        #         print("ApplicationName_box is not enable")
+        # except NoSuchElementException as e:
+        #     print("NoSuchElementException error", e)
+        # else:
+        #     print('Successfully put ApplicationName')
+        #
+        # # scroll down
+        # driver.execute_script("document.querySelector('.sidenav-content').scrollTop = 150")
+        # print("Scroll down")
+        # time.sleep(2)
+        #
+        # # Click on team box
+        # try:
+        #     TeamBox_A = self.driver.find_element(By.XPATH, Locator.TeamBox_A)
+        #     if TeamBox_A.is_enabled():
+        #         print("Team box is Enable")
+        #         TeamBox_A.click()
+        #         time.sleep(2)
+        #     else:
+        #         print("Team box is not Enable")
+        # except NoSuchElementException as e:
+        #     print("NoSuchElementException error", e)
+        # else:
+        #     print('Successfully clicked on TeamBox_A')
+        #
+        # # choose Default from team box
+        # try:
+        #     Team_Default = self.driver.find_element(By.XPATH, Locator.Team_Default)
+        #     if Team_Default.is_displayed():
+        #         print("Team default is selectable")
+        #         Team_Default.click()
+        #         time.sleep(3)
+        #     else:
+        #         print("Team: Default is displayed")
+        # except NoSuchElementException as e:
+        #     print("NoSuchElementException error", e)
+        # else:
+        #     print('Successfully chose on Team Default')
+        #
+        # # scroll below
+        # driver.execute_script("document.querySelector('.sidenav-content').scrollTop = 400")
+        # time.sleep(2)
+        # print("Scroll down")
+        #
+        # #  click next button
+        # try:
+        #     Next_button = self.driver.find_element(By.XPATH, Locator.Next_button)
+        #     if Next_button.is_enabled():
+        #         print("Next button is enable")
+        #         Next_button.click()
+        #         time.sleep(2)
+        #     else:
+        #         print("Next button is not enable")
+        # except NoSuchElementException as e:
+        #     print("NoSuchElementException error", e)
+        # else:
+        #     print('Successfully click on Next_button')
+        #
+        # # Again click next button
+        # try:
+        #     Next_button_two = self.driver.find_element(By.XPATH, Locator.Next_button)
+        #     if Next_button_two.is_enabled():
+        #         print("Next button is enable")
+        #         Next_button_two.click()
+        #         time.sleep(3)
+        #     else:
+        #         print("Next_button_two is not enable")
+        # except NoSuchElementException as e:
+        #     print("NoSuchElementException error", e)
+        # else:
+        #     print('Successfully click on Next_button_two')
+        #
+        # # again scroll below to show Namespaces
+        # driver.execute_script("document.querySelector('.sidenav-content').scrollTop = 250")
+        # time.sleep(2)
+        # print("Scroll down to show Namespaces")
+        #
+        # # Choose A Namespace for Prod Environment
+        # try:
+        #     Choose_Namespace_one = self.driver.find_element(By.XPATH, Locator.Choose_Namespace_one)
+        #     if Choose_Namespace_one.is_enabled():
+        #         print("Namespace is selected")
+        #         Choose_Namespace_one.click()
+        #         time.sleep(5)
+        #     else:
+        #         print("Namespace is not enable")
+        # except NoSuchElementException as e:
+        #     print("NoSuchElementException error", e)
+        # else:
+        #     print('Successfully Choose Namespace one')
+        #
+        # # again scroll below
+        # driver.execute_script("document.querySelector('.sidenav-content').scrollTop = 1200")
+        # time.sleep(2)
+        # print("Scroll down to show Namespaces")
+        #
+        # # click on save button
+        # try:
+        #     Save_button = self.driver.find_element(By.XPATH, Locator.Save_button_A)
+        #     if Save_button.is_enabled():
+        #         print("Save button is enable")
+        #         Save_button.click()
+        #         time.sleep(2)
+        #     else:
+        #         print("Save button is not enable")
+        # except NoSuchElementException as e:
+        #     print("NoSuchElementException error", e)
+        # else:
+        #     print('Successfully clicked on Save button')
+        # # click on Create application button
+        # # try:
+        # #     Create_Application = self.driver.find_element(By.XPATH, Locator.Create_Application)
+        # #     if Create_Application.is_enabled():
+        # #         print("Create application button is enable")
+        # #         Create_Application.click()
+        # #         time.sleep(180)
+        # # except NoSuchElementException as e:
+        # #     print("NoSuchElementException error", e)
+        # # else:
+        # #     print('Successfully clicked on Create_Application')
+        # ss = SS(driver)
+        # file_name = ss_path + "test_Laravel_default_01_scrrenshot_" + time.asctime().replace(":", "_") + ".png"
+        # ss.driver.save_screenshot(file_name)
+        # ss.ScreenShot(file_name)
