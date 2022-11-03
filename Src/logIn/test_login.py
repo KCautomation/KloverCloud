40955c512b382aea17f8bbcbac711d8cd896fd42
MdPorag <mdporagbsmrstu@gmail.com>
2022-11-03 16:46:38 +0600

    Update

--- Src/logIn/test_login.py
+++ Src/logIn/test_login.py
@@ -1,13 +1,17 @@
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from Src.Locators.locators import Locator
 
 from Src.Page_object_model.pom_loginPage import LogInPage
 from Src.Page_object_model.pom_ApplicationPage import CreateApplicationPage
 from Src.screen_shots.screen_shots import SS
 from Src.base.environment_setup import EnvironmentSetup
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from urllib.request import urlopen
 from urllib.error import *
 
@@ -38,18 +42,60 @@ class TestLogIn(EnvironmentSetup):
         driver.get(pageUrl)
         driver.implicitly_wait(20)
         time.sleep(2)
-        username_box = self.driver.find_element(By.XPATH, Locator.Email_box)
-        username_box.send_keys(username)
-        time.sleep(1)
 
-        password_box = self.driver.find_element(By.XPATH, Locator.Password_box)
-        password_box.send_keys(password)
-        time.sleep(1)
+        # put Email
+        try:
+            Email_box = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, Locator.Email_box)))
+            print("Email_box is inputable")
+            Email_box.click()
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        else:
+            print('Successfully put email in Email_box')
 
-        login_box = self.driver.find_element(By.XPATH, Locator.Sign_In_button)
-        login_box.click()
-        time.sleep(5)
-        self.cluster_login(pageUrl, username, password)
+        # put password
+        try:
+            Password_box = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, Locator.Password_box)))
+            print("Password_box is inputable")
+            Password_box.click()
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        else:
+            print('Successfully put password in Password_box')
+
+        # click on Toggle_Visibility_Button
+        try:
+            Toggle_Visibility_Button = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, Locator.Toggle_Visibility_Button)))
+            print("Toggle_Visibility_Button is clickable")
+            Toggle_Visibility_Button.click()
+            time.sleep(1)
+            Toggle_Visibility_Button.click()
+            time.sleep(1)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        else:
+            print('Successfully showed & hided Password')
+        # Click on Sign In button
+
+        try:
+            Sign_In_button = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, Locator.Sign_In_button)))
+            print("Password_box is inputable")
+            Sign_In_button.click()
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        else:
+            print('Successfully click on Sign In button')
 
     # def test1(self):
     #     pageUrl = "https://eks.rakibefstestmaincluster782.klovercloud.io/"
--- pac2.py
+++ pac2.py
@@ -1,30 +1,31 @@
 import time
+from telnetlib import EC
 
 import pytest
+from selenium.common import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.core import driver
 from Src.Locators.locators import Locator
 
 from Src.Page_object_model.pom_loginPage import LogInPage
-from Src.Page_object_model.pom_ApplicationPage import CreateApplicationPage
 from Src.screen_shots.screen_shots import SS
 from Src.base.environment_setup import EnvironmentSetup
-from selenium.common.exceptions import NoSuchElementException
-
 from urllib.request import urlopen
 from urllib.error import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-ss_path = "/Applications/GoLang/"
+ss_path = "/Applications/PHP/"
 
 
-class TestCreateGolangApp(EnvironmentSetup):
-
-    # def test_golang_none_01(self):
-    #     pageUrl = "https://eks.rakibefstestmaincluster782.klovercloud.io/"
-    #     self.driver.get(pageUrl)
-    #     self.driver.implicitly_wait(5)
-    #     self.time.sleep(2)
-
-    def test_golang_default_01(self):
+class TestCreateAppPHP(EnvironmentSetup):
+    def test_Laravel_default_01(self):
+        # pytest.skip("Skipping test...later I will implement...")
         pageUrl = "https://eks.rakibefstestmaincluster782.klovercloud.io/"
         driver = self.driver
 
@@ -48,18 +49,21 @@ class TestCreateGolangApp(EnvironmentSetup):
         self.driver.implicitly_wait(20)
         time.sleep(2)
         # ******************************Login**********************************
-
+        print("----------------------Cluster LogIn-----------------------------------")
         # page object
         log = LogInPage(driver)  # LogIn page
 
         # input email
         try:
-            if log.Email_box.is_enabled():
-                log.Email_box.send_keys('admin@klovercloud.com')
-                print("Email box is enabled")
-                time.sleep(1)
-            else:
-                print("Email box is not enabled")
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located(log.Email_box))
+            log.Email_box.send_keys('admin@klovercloud.com')
+            time.sleep(1)
+            # if log.Email_box.is_enabled():
+            #     print("Email box is enabled")
+            #     log.Email_box.send_keys('admin@klovercloud.com')
+            #     time.sleep(1)
+            # else:
+            #     print("Email box is not enabled")
         except NoSuchElementException as e:
             print("NoSuchElementException error", e)
 
@@ -68,58 +72,61 @@ class TestCreateGolangApp(EnvironmentSetup):
 
         # input pass
         try:
-            if log.Password_box.is_enabled():
-                log.Password_box.send_keys('Hello@1234')
-                print("Password box is enabled")
-                time.sleep(1)
-            else:
-                print("Password box is not enabled")
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located(log.Password_box))
+            log.Password_box.send_keys('Hello@1234')
+            time.sleep(1)
+            # if log.Password_box.is_enabled():
+            #     print("Password box is enabled")
+            #     log.Password_box.send_keys('Hello@1234')
+            #     time.sleep(1)
+            # else:
+            #     print("Password box is not enabled")
         except NoSuchElementException as e:
             print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
         else:
             print('Successfully inputted Password')
 
         # To show password
         try:
-            if log.Toggle_Visibility_Button.is_enabled():
-                log.Toggle_Visibility_Button.click()
-                time.sleep(1)
-                log.Toggle_Visibility_Button.click()
-                print("Password box is enabled")
-                time.sleep(1)
-            else:
-                print("Password box is not enabled")
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located(log.Toggle_Visibility_Button))
+            log.Toggle_Visibility_Button.click()
+            time.sleep(1)
+            log.Toggle_Visibility_Button.click()
+            time.sleep(1)
+            # if log.Toggle_Visibility_Button.is_enabled():
+            #     print("Toggle Visibility Button box is enabled")
+            #     log.Toggle_Visibility_Button.click()
+            #     time.sleep(1)
+            #     log.Toggle_Visibility_Button.click()
+            #     time.sleep(1)
+            # else:
+            #     print("Password box is not enabled")
         except NoSuchElementException as e:
             print("NoSuchElementException error", e)
+
+        except TimeoutException as e:
+            print("TimeoutException error", e)
         else:
             print('Successfully showed & hided Password')
 
         # Click On SignIn button
         try:
-            if log.Sign_In_button.is_enabled():
-                log.Sign_In_button.click()
-                self.driver.implicitly_wait(10)
-                time.sleep(5)
-                print("Sign In button is clickable")
-                time.sleep(2)
-            else:
-                print("Sign In button is not clickable")
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located(log.Sign_In_button))
+            log.Sign_In_button.click()
+            self.driver.implicitly_wait(10)
+            time.sleep(7)
+            # if log.Sign_In_button.is_enabled():
+            #     print("Sign In button is clickable")
+            #     log.Sign_In_button.click()
+            #     self.driver.implicitly_wait(10)
+            #     time.sleep(7)
+            # else:
+            #     print("Sign In button is not clickable")
         except NoSuchElementException as e:
             print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
         else:
             print('Successfully logged in')
-
-        # click on create button from header
-        try:
-            CreateNew_H = self.driver.find_element(By.XPATH, Locator.CreateNew_H)
-            if CreateNew_H.is_enabled():
-                CreateNew_H.click()
-                self.driver.implicitly_wait(5)
-                print("CreateNew_H button is clickable")
-                time.sleep(2)
-            else:
-                print("CreateNew_H In button is not clickable")
-        except NoSuchElementException as e:
-            print("NoSuchElementException error :\n", e, "\n")
-        else:
-            print('Successfully clicked on CreateNew_H')
