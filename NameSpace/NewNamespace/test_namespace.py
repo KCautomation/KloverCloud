24c8a2455b9c75d4e3383990032daaa5e204834d
farihasazid8 <fariha.sazid@klovercloud.com>
2022-11-16 14:05:34 +0600

    few update

--- NameSpace/NewNamespace/test_namespace.py
+++ NameSpace/NewNamespace/test_namespace.py
@@ -523,46 +523,43 @@ class NamespaceCreationOrganization(unittest.TestCase):
 
         # click create button for create
         print("Try to click on Create Button")
-        # try:
-        #     Create_button = WebDriverWait(driver, 10).until(
-        #         EC.presence_of_element_located((By.XPATH,
-        #                                         "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[1]")))
-        #     Create_button.click()
-        #     time.sleep(5)
-        #     WebDriverWait(driver, 120).until(EC.visibility_of_element_located((By.XPATH,
-        #                                                                        "/html/body/kc-root/kc-layout/div/mat-sidenav-container/mat-sidenav-content/main/kc-vpc-list/div/div[2]/div[2]/button[1]/span/div")))
-        # except NoSuchElementException as e:
-        #     print("NoSuchElementException error", e)
-        # except TimeoutException as e:
-        #     print("TimeoutException error", e)
-        # except InvalidSessionIdException as e:
-        #     print("InvalidSessionIdException error", e)
-        #
-        # print("******************Create Namespace Validation**********************")
-        # try:
-        #     actual = driver.current_url
-        #     accepted = "https://eks.alpha.klovercloud.io/namespace"
-        #     if self.assertEqual(actual, accepted):
-        #         print("Created Successfully")
-        #     else:
-        #         print("Created failed")
-        # except AssertionError as e:
-        #     print(e)
+        try:
+            Create_button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[1]")))
+            Create_button.click()
+            time.sleep(5)
+            WebDriverWait(driver, 120).until(EC.visibility_of_element_located((By.XPATH,
+                                                                               "/html/body/kc-root/kc-layout/div/mat-sidenav-container/mat-sidenav-content/main/kc-vpc-list/div/div[2]/div[2]/button[1]/span/div")))
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        print("******************Create Namespace Validation**********************")
+        try:
+            actual = driver.current_url
+            accepted = "https://eks.alpha.klovercloud.io/namespace"
+            if self.assertEqual(actual, accepted):
+                print("Created Successfully")
+            else:
+                print("Created failed")
+        except AssertionError as e:
+            print(e)
 
     @allure.severity(allure.severity_level.CRITICAL)
     def test_team(self):
         # pytest.skip("Skipping test...later I will implement...")
         pageUrl = "https://eks.alpha.klovercloud.io/"
         username = "admin@klovercloud.com"
-        password = "Hello@123"
-        Namespace_Name = "test-26"
+        password = "Hello@1234"
+        Namespace_Name = "test-30"
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.maximize_window()
         driver.get(pageUrl)
         time.sleep(2)
-
-        """ Login """
-
         # try block to read URL
         try:
             html = urlopen(pageUrl)
@@ -653,8 +650,7 @@ class NamespaceCreationOrganization(unittest.TestCase):
                 print('Shown a error message: ',
                       simple_colors.red(LogIn_Authentication_Error.text, ['bold', 'underlined']))
                 print("\n")
-                allure.attach(driver.get_screenshot_as_png(), name="test_login", attachment_type=AttachmentType.PNG)
-                assert False
+                driver.close()
                 # return FileExistsError
             else:
                 pass
@@ -674,11 +670,9 @@ class NamespaceCreationOrganization(unittest.TestCase):
                 Accepted_title = "KloverCloud | Dashboard"
                 self.assertEqual(Dashboard_title, Accepted_title)
                 print("Successfully logged in && Welcome to", Dashboard_title)
-                allure.attach(driver.get_screenshot_as_png(), name="test_login", attachment_type=AttachmentType.PNG)
             else:
-                allure.attach(driver.get_screenshot_as_png(), name="test_login", attachment_type=AttachmentType.PNG)
                 print("Login Failed")
-                assert False
+                allure.attach(driver.get_screenshot_as_png(), name="test_login", attachment_type=AttachmentType.PNG)
         except NoSuchElementException as e:
             print("NoSuchElementException error", e)
         except TimeoutException as e:
@@ -686,49 +680,197 @@ class NamespaceCreationOrganization(unittest.TestCase):
         except InvalidSessionIdException as e:
             print("InvalidSessionIdException error", e)
 
-        """ Create Namespace """
+        print("**********************************Create Namespace With Team******************************")
+
+        # click on create button from header
+        print("Try to click on CreateNew button from Header")
+        try:
+            CreateNew_button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/kc-toolbar[1]/div[1]/button[2]/span[1]")))
+            CreateNew_button.click()
+            driver.implicitly_wait(10)
+            time.sleep(4)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # click namespace
+        print("Try to click on Namespace button from frame")
+        try:
+            Namespace_button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "button[role='menuitem']")))
+            Namespace_button.click()
+            driver.implicitly_wait(10)
+            time.sleep(4)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # input Namespace name
+        print("Try to input Namespace Name")
+        try:
+            NamespaceName_box = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "input[placeholder='Namespace Name']")))
+            NamespaceName_box.send_keys(Namespace_Name)
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # Scroll
+        driver.execute_script("document.querySelector('.sidenav-content').scrollTop = 550")
+        print("Scroll down")
+        time.sleep(2)
+
+        # Choose Team from Access Group
+        print("Try to Choose Team as access group")
+        try:
+            Team = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/button[3]/span[1]/div[1]")))
+            Team.click()
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # click search box and choose organization
+        print("Try to click search box")
+        try:
+            Search_box = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/mat-form-field[1]/div[1]/div[1]/div[4]")))
+            Search_box.click()
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # Organization selection
+        print("Try to choose a team from dropdown")
+        try:
+            Choose_Team = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'default')]")))
+            Choose_Team.click()
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # Scroll
+        driver.execute_script("document.querySelector('.sidenav-content').scrollTop = 730")
+        print("Scroll down")
+        time.sleep(2)
+
+        # CPU selection
+        print("Try to update CPU by input CPU box")
+        try:
+            CPU_box = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]")))
+            CPU_box.send_keys(0)
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # Memory Selection
+        print("Try to update CPU by input Memory box")
+        try:
+            Memory_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
+                                                                                         "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/input[1]")))
+            Memory_box.send_keys(0)
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # Persistent Volume selection
+        print("Try to update Volume by input box")
+        try:
+            Volume_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
+                                                                                         "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/input[1]")))
+            Volume_box.send_keys(0)
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # Bandwidth selection
+        print("Try to update Bandwidth by input box")
+        try:
+            Bandwidth_box = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Moderate')]")))
+            Bandwidth_box.click()
+            time.sleep(2)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        # Scroll
+        driver.execute_script("document.querySelector('.sidenav-content').scrollTop = -730")
+        print("Scroll up")
+        time.sleep(2)
+
+        # click create button for create
+        print("Try to click on Create Button")
+        try:
+            Create_button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[1]")))
+            Create_button.click()
+            time.sleep(5)
+            WebDriverWait(driver, 120).until(EC.visibility_of_element_located((By.XPATH,
+                                                                               "/html/body/kc-root/kc-layout/div/mat-sidenav-container/mat-sidenav-content/main/kc-vpc-list/div/div[2]/div[2]/button[1]/span/div")))
+        except NoSuchElementException as e:
+            print("NoSuchElementException error", e)
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException error", e)
+
+        print("******************Create Namespace Validation**********************")
+        try:
+            actual = driver.current_url
+            accepted = "https://eks.alpha.klovercloud.io/namespace"
+            if self.assertEqual(actual, accepted):
+                print("Created Successfully")
+            else:
+                print("Created failed")
+        except AssertionError as e:
+            print(e)
+
         #
-        # # Click On Namespace From Side Navigation
-        # driver.find_element(By.XPATH, "//span[contains(text(),'Namespace')]").click()
-        # driver.implicitly_wait(10)
-        # time.sleep(6)
-        #
-        # # click on create button from header
-        # driver.find_element(By.XPATH, "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container["
-        #                               "1]/mat-sidenav-content[1]/kc-toolbar[1]/div[1]/button[2]/span[1]").click()
-        # driver.implicitly_wait(10)
-        # time.sleep(4)
-        #
-        # # click namespace
-        # driver.find_element(By.CSS_SELECTOR, "button[role='menuitem']").click()
-        # driver.implicitly_wait(10)
-        # time.sleep(4)
-        #
-        # """
-        # # Choose cluster
-        # driver.find_element(By.XPATH, "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container["
-        #                               "1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div["
-        #                               "1]/div[1]/div[1]/div[2]/div[1]/button[1]/span[1]/img[1]").click()
-        # time.sleep(1)
-        #
-        # """
-        # # input Namespace name
-        # driver.find_element(By.CSS_SELECTOR, "input[placeholder='Namespace Name']").send_keys(Namespace_Name)
-        # time.sleep(3)
-        #
-        # # Choose Team from Access Group
-        # driver.find_element(By.XPATH,
-        #                     "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/button[3]/span[1]/div[1]").click()
-        # time.sleep(2)
-        #
-        # # click on search button
-        # driver.find_element(By.XPATH,
-        #                     "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/kc-vpc-form[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/mat-form-field[1]/div[1]/div[1]/div[4]").click()
-        # time.sleep(2)
-        #
-        # # choose a team
-        # driver.find_element(By.XPATH, "//span[contains(text(),'default')]").click()
-        # time.sleep(2)
+
         #
         # # CPU selection
         # driver.find_element(By.XPATH, "//body/kc-root[1]/kc-layout[1]/div[1]/mat-sidenav-container["
